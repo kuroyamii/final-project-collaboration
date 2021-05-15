2101541f390b4fce89a1762add71bb1eb79d872f
Kuroyamii <gerysastrawan123@gmail.com>
2021-05-15 20:53:00 +0800

    add login and database folder

---
+++ login.c
@@ -0,0 +1,14 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+void signin()
+{
+    char username[51];
+    char username[51];
+    FILE *fptr;
+    printf("Admin Login\n");
+    printf("Username: ");
+    scanf("%[^\n]s",&username);
+    printf("Password: ");
+    scanf("%[^\n]s",&password);
+}
