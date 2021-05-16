b01faf21bf118dd2b1eb10ad5f873850b91a861f
Kuroyamii <gerysastrawan123@gmail.com>
2021-05-16 11:46:36 +0800

    Add global variables, and function eligible, lower, signup, addAdmin

--- login.c
+++ login.c
@@ -3,6 +3,106 @@
 #include "string.h"
 #include "stdbool.h"
 
+char nama[101],email[51],ttl[11],ktp[21];
+char strbuff[101];
+
+char* lower(char str[]){
+   int i;
+   for(i=0;i<=strlen(str);i++){
+      if(str[i]>=65&&str[i]<=90)
+         str[i]=str[i]+32;
+   }
+    return str;
+}
+
+bool eligible(){
+    FILE *fp;
+    char tmp[101],data[101]; 
+    fp = fopen("./databases/dataAdmin.txt","r");
+    fflush(stdin);
+    printf("Nama                     : ");
+    scanf("%[^\n]s",nama);
+    lower(nama);
+    fflush(stdin);
+
+    printf("Email                    : ");
+    scanf("%[^\n]s",email);
+    lower(email);
+    fflush(stdin);
+
+    printf("Tanggal Lahir(DD/MM/YYYY): ");
+    scanf("%[^\n]s",ttl);
+    fflush(stdin);
+
+    printf("No. KTP                  : ");
+    scanf("%[^\n]s",ktp);
+    lower(ktp);
+    fflush(stdin);
+
+    while(fgets(tmp,101,fp)!= NULL){
+        if(strcmp(tmp,"*****\n") == 0){
+            fgets(tmp,101,fp);
+            sscanf(tmp,"%101[^\n]\n", &data);
+            fflush(stdin);
+            if(strcmp(data,nama) == 0){
+                fgets(tmp,51,fp);
+                sscanf(tmp,"%51[^\n]\n", &data);
+                fflush(stdin);
+                if(strcmp(data,email) == 0){
+                    fgets(tmp,11,fp);
+                    sscanf(tmp,"%11[^\n]\n", &data);
+                    fflush(stdin);
+                    if(strcmp(data,ttl) == 0){
+                        fgets(tmp,21,fp);
+                        sscanf(tmp,"%21[^\n]\n", &data);
+                        fflush(stdin);
+                        if(strcmp(data,ktp) == 0){
+                            fclose(fp);
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    fclose(fp);
+    return false;
+}
+
+
+void signup(){
+    FILE *fp;
+    char username[101],password[101],tmp[101],tmp2[101];
+    bool loop = true;
+    fp = fopen("./databases/admin.txt","a+");
+    printf("Masukkan data untuk memeriksa eligibility:\n");
+
+    if(eligible() == true){
+        printf("Masukkan data yang ingin didaftarkan:\n");
+        printf("Username: ");
+        while(loop == true){
+            scanf("%100[^\n]s",username);
+            fflush(stdin);
+            while(fgets(tmp,101,fp)!=NULL){
+                sscanf(tmp,"%[^\n]\n",tmp2);
+                if(strcmp(tmp,tmp2) == 0){
+                    break;
+                }else{
+                    loop = false;
+                }
+            }
+            if(loop == true){
+                printf("Username telah terpakai!\n");
+            }
+        }
+        printf("Password: ");
+        scanf("%100[^\n]s",password);
+        fflush(stdin);
+        fprintf(fp,"*****\n%s\n%s\n",username,password);
+    } else{
+        printf("Maaf anda belum memenuhi kriteria untuk memiliki akun administrator!\n");
+    }
+}
 
 
 bool read(char username[51], char password[51]){
@@ -32,6 +132,7 @@ bool signin()
     char password[51];
     FILE *fptr;
     printf("Admin Login\n");
+    fflush(stdin);
     printf("Username: ");
     scanf("%[^\n]s",&username);
     fflush(stdin);
@@ -40,6 +141,12 @@ bool signin()
     return read(username,password);
 }
 
-int main(){
-    return 0;
+void addAdmin(){
+    printf("Masukkan data admin yang akan ditambah pada database:\n");
+    FILE *fptr;
+    fptr = fopen("./databases/dataAdmin.txt","a");
+    if(eligible() == false){
+        fprintf(fptr,"*****\n%s\n%s\n%s\n%s\n",nama,email,ttl,ktp);
+    }
+    fclose(fptr);
 }
