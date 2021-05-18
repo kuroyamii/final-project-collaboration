18ecc020732b816626fd9c85adcada8ee5450bac
Kuroyamii <gerysastrawan123@gmail.com>
2021-05-18 14:20:57 +0800

    initial commit main.c

--- main.c
+++ main.c
@@ -3,5 +3,16 @@
 #include "stdio.h"
 
 int main(){
-    signin();
+    int pil;
+    do{
+        printf("Pilih Menu:\n");
+        printf("1. Pengunjung\n2. Sign In Admin\n3.Sign Up Admin\n4. Exit\nPilihan: ");
+        scanf("%d",&pil);
+        switch(pil){
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+    }while(pil!=4);
 }
