bb896374faf76a98651288948364c779ea4c0ce2
Kuroyamii <gerysastrawan123@gmail.com>
2021-05-23 20:54:37 +0800

    complete main.c

--- main.c
+++ main.c
@@ -1,26 +1,43 @@
 #include "readPasien.h"
 #include "stdio.h"
+#include "stdlib.h"
+#include "cekCovid.h"
+#include "cekPasien.h"
 #include "stdbool.h"
 
 void clear();
 void pause();
+void title();
+
 
 int main(){
     int pil,pil2,pil3;
     bool loop = true;
     do{
+        title();
         printf("Pilih Menu:\n");
         printf("1. Pengunjung\n2. Sign In Admin\n3. Sign Up Admin\n4. Exit\nPilihan: ");
         scanf("%d",&pil);
+        title();
         switch(pil){
             case 1:
+            {
+                if(cekCovid() == true)
+                {
+                    cekPasien();
+                }else{
+                    exit(1);
+                }
                 break;
+            }
             case 2:
             {
                 if(signin() == true){
                     while(1){
+                        title();
                         printf("1. Tambah Admin\n2. Menu Pasien\n3. Log Out\nPilihan: ");
                         scanf("%d",&pil2);
+                        title();
                         switch(pil2){
                             case 1:
                                 addAdmin();
@@ -28,8 +45,10 @@ int main(){
                             case 2:
                             {
                                 while(1){
+                                    title();
                                     printf("1. Read Data Pasien\n2. Update Data Pasien\n3. Selesai\nPilihan: ");
                                     scanf("%d",&pil3);
+                                    title();
                                     switch(pil3){
                                         case 1:
                                             printPasien();
@@ -63,7 +82,7 @@ int main(){
             default:
                 break;
         }
-        if (pil == 1 || pil == 2 || pil == 3) pause();
+        if (pil == 1 || pil == 2 || pil == 3 ) pause();
     }while(loop == true);
 }
 
@@ -86,4 +105,11 @@ void pause(){
             break;
         }else if(strcmp(pil,"y") == 0 || strcmp(pil,"Y") == 0) break;
     }
+}
+
+void title(){
+    clear();
+    printf("+----------------------------------------------------------+\n");
+    printf("|          PROGRAM SIMULASI RUMAH SAKIT SPESIALIS          |\n");
+    printf("+----------------------------------------------------------+\n");
 }
